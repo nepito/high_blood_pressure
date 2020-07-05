ad36e74651514b3c446350f8f6c05853c2dee9da
nepo <braulio.rojas@islas.org.mx>
2020-07-04 18:53:31 -0700

    Pasa la segunda prueba

--- highBloodPressure/R/Data_HP.R
+++ highBloodPressure/R/Data_HP.R
@@ -1,13 +1,11 @@
 library(R6)
-datos <- tibble::tibble(
-    date = c('2020-01-01', '2020-01-02'),
-    pressure = c(120, 140)
-)
 Data_HP <- R6Class("Data_HP", list(
+    data = NULL,
     get_data = function(){
-        return(datos)
+        return(self$data)
     },
     set_data = function(x){
+        self$data <- x
     }
   )
 )
