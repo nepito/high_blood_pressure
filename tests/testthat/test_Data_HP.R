b5466aad16aaf8e7cd9325c6ba65e087c102a36c
nepo <braulio.rojas@islas.org.mx>
2020-07-04 17:41:09 -0700

    Agrega los datos para probar

--- tests/testthat/test_Data_HP.R
+++ tests/testthat/test_Data_HP.R
@@ -1,4 +1,8 @@
-library("high_blood_pressure")
+library("highBloodPressure")
+data <- tibble::tibble(
+    date = c('2020-01-01', '2020-01-02'),
+    pressure = c(120, 140)
+)
 test_that("La clase pueda cargar los datos", {
     expect_true(FALSE)
 })
