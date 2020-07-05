54f8f7ecb05cbe8ed72adff01eaf5cc80040a406
nepo <braulio.rojas@islas.org.mx>
2020-07-04 18:14:02 -0700

    Prueba setter/getter de la propiedad data

--- tests/testthat/test_Data_HP.R
+++ tests/testthat/test_Data_HP.R
@@ -4,6 +4,7 @@ data <- tibble::tibble(
     pressure = c(120, 140)
 )
 Blood_Pressure <- highBloodPressure::Data_HP$new()
+Blood_Pressure$set_data(data)
 test_that("La clase pueda cargar los datos", {
-    expect_true(FALSE)
+    expect_equal(Blood_Pressure$get_data, data)
 })
